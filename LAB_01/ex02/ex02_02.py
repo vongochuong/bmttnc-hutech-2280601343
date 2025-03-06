04bb6feeaf9913ad2deffc6c35dcecbf6f9a7f54
vongochuong <vongochuong01092000@gmail.com>
2025-03-06 15:24:07 +0700

    lab01-ex02

---
+++ LAB_01/ex02/ex02_02.py
@@ -0,0 +1,3 @@
+ban_kinh = float (input("nhap ban kinh hinh tron "))
+dientich= 3.14*(ban_kinh **2)
+print("diem tich cua hinh tron :",dientich)
---
+++ LAB_01/ex02/ex02_03.py
@@ -0,0 +1,5 @@
+so = int (input("nhap mot so nguyen "))
+if so%2 ==0:
+    print(so,"la chan")
+else:
+    print(so,"la le")
